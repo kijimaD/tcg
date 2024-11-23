4a6e8e5866a47ba59a633210bcadf67a8364c44d
Kijima Daigo <norimaking777@gmail.com>
2024-11-24 00:02:15 +0900

    feat: デザイン調整した

--- main.go
+++ main.go
@@ -11,6 +11,7 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -19,11 +20,9 @@ import (
 )
 
 func main() {
-	exec()
-
 	fileServer := http.FileServer(http.Dir("."))
 	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
-	// http.Handle("/circle", http.HandlerFunc(circle))
+	http.Handle("/circle", http.HandlerFunc(circle))
 	err := http.ListenAndServe(":2003", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
@@ -36,41 +35,29 @@ func circle(w http.ResponseWriter, req *http.Request) {
 	const padding = 10
 
 	w.Header().Set("Content-Type", "image/svg+xml")
-	s := svg.New(w)
-	s.Start(width, height)
-
-	// 全体枠
-	s.Rect(0, 0, width/2+padding*2, height/2+padding*2, "fill:none;stroke:black;")
-	// タイトル背景
-	s.Rect(0, 0, width/2+padding*2, 36, "fill:#000;fill-opacity:0.5;stroke:black;")
-	s.Text(width/4, 30, "橋跡", "text-anchor:middle;font-size:16px;fill:black")
-	s.Text(padding, 30+30+height/4+padding*2+30, "旧陣之尾橋跡", "font-size:16px;fill:black")
-	s.Image(padding, 30+30, width/2, height/4+padding*2, "/static/image.png")
-	s.Rect(padding, 30+30, width/2, height/4+padding*2, "fill:none;stroke:black;")
-	s.End()
-}
-
-func exec() {
-	const width = 500
-	const height = 800
-	const padding = 10
-
 	f, err := os.Create("test.svg")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
-
-	s := svg.New(f)
-	s.Start(width/2, height/2)
+	mw := io.MultiWriter(f, w)
+	s := svg.New(mw)
+	s.Start(width/2+padding*2, height/2+padding*2)
 
 	// 全体枠
-	s.Rect(0, 0, width/2+padding*2, height/2+padding*2, "fill:none;stroke:black;")
-	// タイトル背景
-	s.Rect(0, 0, width/2+padding*2, 36, "fill:#000;fill-opacity:0.5;stroke:black;")
-	s.Text(width/4, 30, "橋跡", "text-anchor:middle;font-size:16px;fill:black")
-	s.Text(padding, 30+30+height/4+padding*2+30, "旧陣之尾橋跡", "font-size:16px;fill:black")
-	s.Image(padding, 30+30, width/2, height/4+padding*2, "/static/image.png")
-	s.Rect(padding, 30+30, width/2, height/4+padding*2, "fill:none;stroke:black;")
+	s.Rect(0, 0, width/2+padding*2, height/2+padding*2, "fill:royalblue;rx:10;ry:10;")
+
+	// 画像
+	s.Image(padding, padding*2, width/2, height/4+padding*2, "/static/image.png")
+	s.Rect(padding, padding*2, width/2, height/4+padding*2, "fill:none;")
+
+	// 本文
+	s.Rect(padding, 30+height/4, width/2, 16*11, "fill:white;fill-opacity:0.9;rx:5;ry:5")
+	s.Text(padding*2, height/4+padding*6, "旧陣之尾橋跡", "font-size:16px;fill:black")
+
+	// タイトル
+	s.Rect(0, padding, width/2+padding*2, 16*2, "fill:white;fill-opacity:1.0;")
+	s.Text(width/4, 16*2, "橋跡", "text-anchor:middle;font-size:16px;fill:black")
+
 	s.End()
 }
