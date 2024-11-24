02c6305dd91887bf4be3bc61ef2800c0f0ff73e2
Kijima Daigo <norimaking777@gmail.com>
2024-11-24 10:40:20 +0900

    feat: functionize

--- main.go
+++ main.go
@@ -36,18 +36,41 @@ func main() {
 
 	fileServer := http.FileServer(http.Dir("."))
 	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
-	http.Handle("/build", http.HandlerFunc(build))
+	http.Handle("/check", http.HandlerFunc(check))
 	err = http.ListenAndServe(":2003", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
 
-func build(w http.ResponseWriter, req *http.Request) {
+func build(w io.Writer) {
 	const width = 500
 	const height = 800
 	const padding = 10
 
+	s := svg.New(w)
+	s.Start(width/2+padding*2, height/2+padding*2)
+
+	// 全体枠
+	s.Rect(0, 0, width/2+padding*2, height/2+padding*2, "fill:royalblue;rx:10;ry:10;")
+
+	// 画像
+	s.Image(padding, padding+16*2, 250, 250, fmt.Sprintf("data:image/png;base64,%s", imageBase("./normalize.png")))
+	// 画像枠
+	s.Rect(padding, padding+16*2, width/2, 250, "fill:none;stroke:gold;")
+
+	// 本文
+	s.Rect(padding, height/4+padding*6, width/2, 16*8, "fill:white;fill-opacity:1.0;rx:8;ry:8")
+	s.Text(padding*2, height/4+padding*10, "橋台が残っている", "font-size:16px;fill:black")
+
+	// タイトル
+	s.Rect(0, padding, width/2+padding*2, 16*2, "fill:white;fill-opacity:1.0;stroke:black;")
+	s.Text(width/4, 16*2, "旧陣之尾橋跡", "text-anchor:middle;font-size:16px;fill:black;")
+
+	s.End()
+}
+
+func check(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "image/svg+xml")
 	f, err := os.Create("test.svg")
 	if err != nil {
@@ -55,25 +78,7 @@ func build(w http.ResponseWriter, req *http.Request) {
 	}
 	defer f.Close()
 	mw := io.MultiWriter(f, w)
-	s := svg.New(mw)
-	s.Start(width/2+padding*2, height/2+padding*2)
-
-	// 全体枠
-	s.Rect(0, 0, width/2+padding*2, height/2+padding*2, "fill:royalblue;rx:10;ry:10;")
-
-	// 画像
-	s.Image(padding, padding*4, width/2, height/4+padding*2, fmt.Sprintf("data:image/png;base64,%s", imageBase("./normalize.png")))
-	s.Rect(padding, padding*2, width/2, height/4+padding*2, "fill:none;")
-
-	// 本文
-	s.Rect(padding, height/4+padding*6, width/2, 16*8, "fill:white;fill-opacity:0.8;rx:5;ry:5")
-	s.Text(padding*2, height/4+padding*8, "旧陣之尾橋跡", "font-size:16px;fill:black")
-
-	// タイトル
-	s.Rect(0, padding, width/2+padding*2, 16*2, "fill:white;fill-opacity:1.0;")
-	s.Text(width/4, 16*2, "橋跡", "text-anchor:middle;font-size:16px;fill:black")
-
-	s.End()
+	build(mw)
 }
 
 func imageBase(src string) string {
